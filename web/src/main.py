560da7f8da07a199ce3d246b144b8aaedd9e578b
gruzilkin <gruzmob@gmail.com>
2025-07-14 00:50:56 +0900

    set default port to 80

--- web/src/main.py
+++ web/src/main.py
@@ -139,5 +139,5 @@ async def shutdown_event():
     await store.close()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "8000"))
+    port = int(os.getenv("PORT", "80"))
     uvicorn.run(app, host="0.0.0.0", port=port)
